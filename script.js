5eb251333d6e73a0be979c2e031f93b027b88907
Egor Komarovskiy <bishep.ok@yandex.ru>
2024-12-03 21:05:09 +0300

    Wrote functions for the game

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 const btn = document.getElementById("show-button");
 const list = document.getElementById("result");
+const getRandomNumber = () => Math.floor(Math.random() * 100 + 1);
+
+const guessRandomNumber = (randomNumber) => {
+  let min = 1;
+  let max = 100;
+  let attempts = 0;
+
+  while (min <= max) {
+    attempts++;
+    const attempt = Math.floor((min + max) / 2);
+    console.log(`Компьютер 2: Пробую число ${attempt}.`);
+
+    if (attempt === randomNumber) {
+      console.log("Компьютер 1: Угадал!");
+      return attempts;
+    } else if (attempt < randomNumber) {
+      console.log("Компьютер 1: Больше.");
+      min = attempt + 1;
+    } else {
+      max = attempt - 1;
+      console.log("Компьютер 1: Меньше.");
+    }
+  }
+};
+
+const randomNumber = getRandomNumber();
+console.log(`Компьютер 1 загадал число ${randomNumber}.`);
+
+const attemptsCount = guessRandomNumber(randomNumber);
+console.log(`Компьютер 2 угадал число за ${attemptsCount} попыток`);
